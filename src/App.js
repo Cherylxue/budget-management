adf1b1bc12af279eb2735db755ac5f268f89b07c
Chun Xue <49353300+Cherylxue@users.noreply.github.com>
2021-06-27 15:23:47 -0400

    setup firestore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBbFCLxkbTt8nFLsGO-_KQMW-B-MTwPSdY",
+    authDomain: "budget-management-dd231.firebaseapp.com",
+    projectId: "budget-management-dd231",
+    storageBucket: "budget-management-dd231.appspot.com",
+    messagingSenderId: "214832321974",
+    appId: "1:214832321974:web:dbb46ae7c51dd931525d17",
+    measurementId: "G-7EQ6WLB2Y1",
+  };
+
+  firebase.initializeApp(firebaseConfig);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
